86f0a5ac2995217e3de8b7396a0b29cf71c6e365
mikaeleythor <eythor108@gmail.com>
2023-05-23 17:49:49 +0000

    Cleanup

--- firesale/settings.py
+++ firesale/settings.py
@@ -131,9 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
 
 
 STORAGES = {
