a142c0c8b9bd1a079ce66c67de1b43c5dd48b0ab
Rannveig Sigurgeirsdottir <rannveigsigurgeirsdottir@Rannveigs-MacBook-Pro-2.local>
2023-05-25 16:33:54 +0000

    nytt create item

--- item/forms/item_form.py
+++ item/forms/item_form.py
@@ -4,27 +4,36 @@ from item.models import Item
 
 
 class ItemCreateForm(ModelForm):
-    image = forms.CharField(required=True, widget=forms.TextInput(attrs={ 'class':'form-control' }))
+    image = forms.CharField(label="", required=True, widget=forms.TextInput(
+        attrs={'placeholder': '+ Add image'}))
+
     class Meta:
         model = Item
-        exclude = [ 'id', 'seller', 'status']
+        exclude = ['id', 'seller', 'status']
+        labels = {
+            'name': "",
+            'condition': "",
+            'price': "",
+            'category': "",
+            'description': ""
+        }
         widgets = {
-            'name': widgets.TextInput(attrs={ 'class':'form-control' }),
-            'description': widgets.TextInput(attrs={ 'class':'form-control' }),
-            'category': widgets.TextInput(attrs={'class': 'form-control'}),
-            'price': widgets.TextInput(attrs={ 'class':'form-control' }),
-            'condition': widgets.TextInput(attrs={'class': 'form-control'}),
+            'name': widgets.TextInput(attrs={'placeholder': 'Name'}),
+            'condition': widgets.TextInput(attrs={'placeholder': 'Condition'}),
+            'price': widgets.TextInput(attrs={'placeholder': 'Price'}),
+            'category': widgets.TextInput(attrs={'placeholder': 'Category'}),
+            'description': widgets.TextInput(attrs={'placeholder': 'Description'}),
         }
 
 
 class ItemUpdateForm(ModelForm):
     class Meta:
         model = Item
-        exclude = [ 'id', 'seller', 'status']
+        exclude = ['id', 'seller', 'status']
         widgets = {
-            'name': widgets.TextInput(attrs={ 'class':'form-control' }),
-            'description': widgets.TextInput(attrs={ 'class':'form-control' }),
+            'name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'description': widgets.TextInput(attrs={'class': 'form-control'}),
             'category': widgets.TextInput(attrs={'class': 'form-control'}),
-            'price': widgets.TextInput(attrs={ 'class':'form-control' }),
+            'price': widgets.TextInput(attrs={'class': 'form-control'}),
             'condition': widgets.TextInput(attrs={'class': 'form-control'}),
         }
