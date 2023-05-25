06c2b7bf317e78c7b0316728213eca255699337c
Bríet Eva Gísladóttir <brieteva@hotmail.com>
2023-05-25 16:51:47 +0000

    Styled the profile view

--- person/views.py
+++ person/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render
 from person.models import Person
-# Create your views here.
-
-# persons = [
-#     {'name': 'smarties', 'price': 100},
-#     {'name': 'skittles', 'price': 200}
-# ]
 
 
 def index(request):
     # context = {'persons': persons}
     # return render(request, 'person/index.html', context)
+
     context = {'persons': Person.objects.all()}
     return render(request, 'person/index.html', context)
