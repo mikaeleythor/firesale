44b155f4d5843c2ced7c6877d9afcae0965d5a44
Bríet Eva Gísladóttir <brieteva@hotmail.com>
2023-05-25 17:39:58 +0000

    Took some comments that were still

--- person/views.py
+++ person/views.py
@@ -3,8 +3,5 @@ from person.models import Person
 
 
 def index(request):
-    # context = {'persons': persons}
-    # return render(request, 'person/index.html', context)
-
     context = {'persons': Person.objects.all()}
     return render(request, 'person/index.html', context)
