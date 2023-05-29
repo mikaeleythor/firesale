d2a9438c95b49e977095b90856ca99fb1786ed10
mikaeleythor <eythor108@gmail.com>
2023-05-29 11:14:57 +0000

    create notification model, note presence of 'title' which is not in the design

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Notification(models.Model):
+    title = models.CharField(max_length=50)
+    message = models.CharField(max_length=256)
+    date = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
