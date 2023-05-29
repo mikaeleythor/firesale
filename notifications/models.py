e8b766658f619c99aeb0b18e6ef30956822c7a07
mikaeleythor <eythor108@gmail.com>
2023-05-29 11:41:38 +0000

    added a string function

--- notifications/models.py
+++ notifications/models.py
@@ -8,3 +8,6 @@ class Notification(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     read = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title}-{self.date}'
