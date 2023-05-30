da45069e4b9c97848b943d9677891c2ff0806054
Bríet Eva Gísladóttir <brieteva@hotmail.com>
2023-05-30 11:54:55 +0000

    Adding create person functionality

--- accounts/views.py
+++ accounts/views.py
@@ -5,5 +5,5 @@ from django.views import generic
 
 class RegisterView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy("profile/create_person")
     template_name = "registration/signup.html"
--- item/admin.py
+++ item/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from item.models import Item, ItemImage, Seller, Offer
 
-# Register your models here.
 admin.site.register(Item)
 admin.site.register(ItemImage)
 admin.site.register(Seller)
