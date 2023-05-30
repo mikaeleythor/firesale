f1cb776eca8cd45409956d68b943712a1cc6692a
Bríet Eva Gísladóttir <brieteva@hotmail.com>
2023-05-30 19:43:26 +0000

    Redirection fixed

--- accounts/views.py
+++ accounts/views.py
@@ -5,5 +5,5 @@ from django.views import generic
 
 class RegisterView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("profile/create_person")
+    success_url = reverse_lazy("create-person")
     template_name = "registration/signup.html"
