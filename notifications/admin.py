8a13b7b572e75fada498cfd7d84bb0c5e5cc53ca
mikaeleythor <eythor108@gmail.com>
2023-05-30 15:58:56 +0000

    Created a new inbox model to extend the user model, this is done so the "unread" variable is easily accessible in the templates as user.inbox.unread

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from notifications.models import Notification
+from notifications.models import Notification, Inbox
 
 # Register your models here.
 admin.site.register(Notification)
+admin.site.register(Inbox)
