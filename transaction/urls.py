004ee49a747f953298f68634354107e41b0ae1be
saraatla <saraatlala@gmail.com>
2023-05-30 22:14:14 +0000

    basket finished + starting on contact info

--- transaction/urls.py
+++ transaction/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="transaction-index")
+    path('', views.index, name="transaction-index"),
+    path('contact-information', views.contact_information,
+         name="contact-information")
 ]
--- transaction/views.py
+++ transaction/views.py
@@ -4,6 +4,15 @@ from django.shortcuts import render
 
 
 def index(request):
-    accepted_offers = request.user.offer_set.filter(status='pending')
-    print(accepted_offers)
+    accepted_offers = request.user.offer_set.filter(status='Accepted')
     return render(request, 'checkout/index.html', {'basket': accepted_offers})
+
+
+def contact_information(request):
+    if request.method == 'POST':
+        request.session['contact_info'] = {
+            'full_name': request.POST.get('full_name'),
+            'city': request.POST.get('city')
+        }
+
+    return render(request, 'checkout/contact_information.html')
