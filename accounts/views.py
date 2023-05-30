ac62ccb79b9035477541bef37523edfb88bc92fe
Bríet Eva Gísladóttir <brieteva@hotmail.com>
2023-05-30 20:55:09 +0000

    REfactoring like shit

--- accounts/views.py
+++ accounts/views.py
@@ -5,5 +5,5 @@ from django.views import generic
 
 class RegisterView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("create-person")
+    success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
