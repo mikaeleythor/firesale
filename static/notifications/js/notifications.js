8acb4544509090a21dc5c841c88925926a4a10e3
mikaeleythor <eythor108@gmail.com>
2023-05-31 13:27:22 +0000

    Implemented read functionality in template, along with redirection to relevant resources from notification click

---
+++ static/notifications/js/notifications.js
@@ -0,0 +1,36 @@
+const handleNotification = (btn) => {
+	// NOTE: Send empty post request to notifications/:id to update read status
+	const notificationId = btn.id.toString().split("-")[2];
+	axios
+		.post(
+			`/notifications/${notificationId}`,
+			{},
+			{
+				headers: {
+					// HACK: Django throws 403 without this explicit csrf header
+					"X-CSRFToken": getCookie("csrftoken"),
+					// HACK: Better than using multipart/form-data since this is not form
+					//       This needed explicit configuration in view
+					"content-type": "application/json",
+				},
+			}
+		)
+		.then((res) => {
+			console.log(res.data);
+		})
+		.catch((e) => {
+			console.error(e);
+		});
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+	const notificationBtns = document.querySelectorAll("[id^=notification-btn-]");
+
+	if (notificationBtns) {
+		notificationBtns.forEach((btn) => {
+			btn.addEventListener("click", (e) => {
+				handleNotification(btn);
+			});
+		});
+	}
+});
