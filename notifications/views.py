340701f1cc000fe6cca241798394294286642d37
mikaeleythor <eythor108@gmail.com>
2023-05-31 13:24:48 +0000

    Implemented a read() endpoint for notifications

--- notifications/views.py
+++ notifications/views.py
@@ -1,5 +1,7 @@
+from django.http import JsonResponse
 from django.views.generic import ListView
 from notifications.models import Notification, Inbox
+from notifications.services import NotificationService
 
 
 class NotificationListView(ListView):
@@ -9,3 +11,10 @@ class NotificationListView(ListView):
     def get_queryset(self):
         inbox, created = Inbox.objects.get_or_create(user=self.request.user)
         return Notification.objects.filter(inbox=inbox.id)
+
+
+def read(request, id):
+    if request.method == 'POST':
+        NotificationService().read(id)
+        return JsonResponse(status=200, data={"message": "Message read"})
+    return JsonResponse(status=400, data={"message": "Unsupported method"})
