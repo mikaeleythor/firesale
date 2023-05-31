2ac73081a0f3429dc07df59ad1f1983305f7626d
Bríet Eva Gísladóttir <brieteva@hotmail.com>
2023-05-31 13:32:38 +0000

    Fixing comment

--- person/forms/person_form.py
+++ person/forms/person_form.py
@@ -3,7 +3,7 @@ from django import forms
 
 class PersonUpdateForm(forms.Form):
     first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=0)
+    last_name = forms.CharField(max_length=50)
     bio = forms.CharField(widget=forms.Textarea(
         attrs={'cols': 60, 'rows': 5}))
     image = forms.ImageField()
