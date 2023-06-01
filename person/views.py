526912d9519203485435a5c7e99c74341547a5b9
mikaeleythor <eythor108@gmail.com>
2023-06-01 22:32:53 +0000

    closes #173

--- person/views.py
+++ person/views.py
@@ -10,36 +10,39 @@ def index(request):
     return render(request, 'person/index.html', context)
 
 
+@login_required
 def update_person(request):
-    id = request.user.person.id
-    if request.method == 'POST':
-        form = PersonUpdateForm(request.POST, request.FILES)
-        if form.is_valid():
-            person_instance = Person.objects.get(id=id)
-            person_instance.bio = form.cleaned_data['bio']
-            person_instance.image = form.cleaned_data['image']
-            person_instance.save()
-            User.objects.filter(id=request.user.id).update(
-                first_name=form.cleaned_data['first_name'],
-                last_name=form.cleaned_data['last_name'],
-                email=form.cleaned_data['email']
-            )
-            return redirect('person-index')
-    else:
-        form = PersonUpdateForm()
-    return render(request, 'person/update_profile.html', {
-        'form': form,
-        'id': id
-    })
+    if hasattr(request.user, 'person'):
+        id = request.user.person.id
+        if request.method == 'POST':
+            form = PersonUpdateForm(request.POST, request.FILES)
+            if form.is_valid():
+                person_instance = Person.objects.get(id=id)
+                person_instance.bio = form.cleaned_data['bio']
+                person_instance.image = form.cleaned_data['image']
+                person_instance.save()
+                User.objects.filter(id=request.user.id).update(
+                    first_name=form.cleaned_data['first_name'],
+                    last_name=form.cleaned_data['last_name'],
+                    email=form.cleaned_data['email']
+                )
+                return redirect('person-index')
+        else:
+            form = PersonUpdateForm()
+        return render(request, 'person/update_profile.html', {
+            'form': form,
+            'id': id
+        })
+    return redirect('/profile/create-person')
 
 
 @login_required
 def create_person(request):
-    if request.user.person is None:
+    if not hasattr(request.user, 'person'):
         if request.method == 'POST':
             form = PersonUpdateForm(request.POST, request.FILES)
             if form.is_valid():
-                person_instance = Person.objects.create(
+                Person.objects.create(
                     user_id=request.user.id,
                     bio=form.cleaned_data['bio'],
                     image=form.cleaned_data['image']
