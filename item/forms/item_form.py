6f987f084d169e9c4952f29a1adfcd27393dd9b3
Rannveig Sigurgeirsdottir <rannveigsigurgeirsdottir@Rannveigs-MacBook-Pro-2.local>
2023-06-01 22:18:11 +0000

    Putting max value on modelform in item_form

--- item/forms/item_form.py
+++ item/forms/item_form.py
@@ -2,37 +2,26 @@ from django import forms
 from django.forms import ModelForm, widgets, Form
 from item.models import Item
 
-class ItemCreateForm(ModelForm):
+class ItemCreateForm(forms.ModelForm):
+    name = forms.CharField(label="",max_length=25, widget=forms.TextInput(attrs={'placeholder': 'Name'}))
+    condition = forms.CharField(label="",max_length=40, widget=forms.TextInput(attrs={'placeholder': 'Condition'}))
+    price = forms.CharField(label="",max_length=40, widget=forms.TextInput(attrs={'placeholder': 'Price'}))
+    category = forms.CharField(label="",max_length=40, widget=forms.TextInput(attrs={'placeholder': 'Category'}))
+    description = forms.CharField(label="",max_length=200, widget=forms.TextInput(attrs={'placeholder': 'Description'}))
     class Meta:
         model = Item
         exclude = ['id', 'seller', 'status']
-        labels = {
-            'name': "",
-            'condition': "",
-            'price': "",
-            'category': "",
-            'description': ""
-        }
-        widgets = {
-            'name': widgets.TextInput(attrs={'placeholder': 'Name'}),
-            'condition': widgets.TextInput(attrs={'placeholder': 'Condition'}),
-            'price': widgets.TextInput(attrs={'placeholder': 'Price'}),
-            'category': widgets.TextInput(attrs={'placeholder': 'Category'}),
-            'description': widgets.TextInput(attrs={'placeholder': 'Description'}),
-        }
 
 
 class ItemUpdateForm(ModelForm):
+    name = forms.CharField(label="",max_length=25, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    condition = forms.CharField(label="",max_length=40, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    price = forms.CharField(label="",max_length=40, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    category = forms.CharField(label="",max_length=40, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    description = forms.CharField(label="",max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'} ))
     class Meta:
         model = Item
         exclude = ['id', 'seller', 'status']
-        widgets = {
-            'name': widgets.TextInput(attrs={'class': 'form-control'}),
-            'description': widgets.TextInput(attrs={'class': 'form-control'}),
-            'category': widgets.TextInput(attrs={'class': 'form-control'}),
-            'price': widgets.TextInput(attrs={'class': 'form-control'}),
-            'condition': widgets.TextInput(attrs={'class': 'form-control'}),
-        }
 
 class ItemOfferForm(Form):
     amount = forms.IntegerField(min_value=1, label="", widget=forms.TextInput(
