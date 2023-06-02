25196562ed84076ef5e4f04d4f41c62416ddd8a7
saraatla <saraatlala@gmail.com>
2023-06-02 00:19:45 +0000

    Ability to see other peoples profile

--- person/urls.py
+++ person/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # http://localhost:8000/person
     path('', views.index, name="person-index"),
     path('update', views.update_person, name='update-person'),
-    path('create-person', views.create_person, name='create-person')
+    path('create-person', views.create_person, name='create-person'),
+    path('<int:id>', views.get_profile_by_id, name='profile-details'),
 ]
